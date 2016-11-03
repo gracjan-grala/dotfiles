a19fe87ec765811057cdc105ab55915b2aaf8ac4
Gracjan Grala <gracjan.grala@gmail.com>
2016-11-03 14:07:28 +0100

    [git] add alias for git fetch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,3 +21,4 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grh='git reset --hard'
 alias gsu='git branch --set-upstream-to=origin/`git symbolic-ref --short HEAD`'
 alias gaa='git add -A'
+alias gf='git fetch'
