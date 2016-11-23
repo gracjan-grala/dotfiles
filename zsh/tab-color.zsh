0757c825e9546d9eefad2427d1f30da2619f618d
Gracjan Grala <gracjan.grala@gmail.com>
2016-11-23 02:29:50 +0100

    [zsh] - use yellow tab color for node-related things - delete unused alias cls

--- zsh/tab-color.zsh
+++ zsh/tab-color.zsh
@@ -1,7 +1,7 @@
 # Decorate terminal tab color based on currently running app
 
 alias tab-red="tab-color 203 111 111"
-alias tab-green="tab-color 6cc276"
+alias tab-green="tab-color 4a4"
 alias tab-yellow="tab-color \"#e8e9ac\""
 alias tab-blue="tab-color 6f8ccc"
 alias tab-purple="tab-color a789d4"
@@ -11,8 +11,9 @@ alias tab-grey="tab-color bcbcbc"
 
 set_tab_color() {
   case "$1" in
-    vi* ) tab-color 4a4 ;;
+    vi* ) tab-green ;;
     (pry|irb|ruby|bundle|rake|rails|guard|rspec|m\ )* ) tab-red ;;
+    (node|npm|nvm)* ) tab-yellow ;;
     vagrant* ) tab-purple ;;
     ssh* ) tab-orange ;;
     vpn ) tab-white ;;
