1524a78c97fa7fd41b1be319ab816e3cddaae24c
Gracjan Grala <gracjan.grala@gmail.com>
2017-01-27 11:28:29 +0700

    [git] Add alias gmine for showing history of my commits -     Add alias gbranch for listing branches sorted chronologically -     Update older aliases with some options

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,16 +9,18 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glr='git pull --rebase --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog='git log --graph --pretty=format:"%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset" --abbrev-commit --date=relative'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gdc='git diff --cached'
-alias gc='git commit'
+alias gc='git commit -n'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
-alias gb='git branch'
+alias gb='git branch --sort=-committerdate'
+alias gbranch='git for-each-ref --sort=-committerdate refs/heads/ --format="%(HEAD) %(color:yellow)%(refname:short)%(color:reset) * %(color:red)%(objectname:short)%(color:reset) %(authorname): %(contents:subject) (%(color:green)%(committerdate:relative)%(color:reset))"'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grh='git reset --hard'
 alias gsu='git branch --set-upstream-to=origin/`git symbolic-ref --short HEAD`'
 alias gaa='git add -A'
 alias gf='git fetch'
+alias gmine='glog --all --date-order --author=Gracjan'
