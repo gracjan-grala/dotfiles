fa6edb5024dd0fa30120cd24a3466cc64645fdb8
Gracjan Grala <gracjan@gra.la>
2018-11-03 00:03:22 +0100

    Some new aliases * sandbox settings for virtualenv (python) * remove rbenv * vim plugins and settings

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -14,3 +14,5 @@ alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 alias password='openssl rand -base64 30'
 alias macaddr="openssl rand -hex 6 | sed 's%\(..\)%\1:%g; s%.$%%'"
 alias spoofmac='sudo ifconfig en0 ether `macaddr`'
+alias launch-items='l ~/Library/LaunchAgents ~/Library/LaunchDaemons /Library/LaunchAgents /Library/LaunchDaemons'
+alias startup-items=launch-items
